4367e1041822fc0fdd5f6ddc136d7125697fddc6
Bob Z Saludo <bob-zs@users.noreply.github.com>
2022-07-12 20:52:16 -0400

    Thing page displays higher rankings at top

--- src/Things.js
+++ src/Things.js
@@ -17,33 +17,35 @@ const Things = ({ things, users, deleteThing, increment, updateThing })=> {
       <h1>Things</h1>
       <ul>
         {
-          things.map( thing => {
-            const owner = usersWithNumThings.find(user => user.id === thing.userId) || {};
-            return (
-              <li key={ thing.id }>
-                { thing.name } ({ thing.ranking })
-                { owner.name && ` owned by ${owner.name} who has ${owner.numThingsOwned} thing${ (owner.numThingsOwned > 1) ? `s` : '' }` || `` }
-                <div>
-                  <select defaultValue={ thing.userId } onChange={ ev => updateThing(thing, ev.target.value )}>
-                    <option value=''>-- nobody --</option>
-                    {
-                      usersWithNumThings
-                        .filter(user => user.numThingsOwned < 3 || user.id === thing.userId)
-                        .map( user => {
-                        return (
-                          <option key={ user.id } value={ user.id }>{ user.name }</option>
-                        );
-                      })
-                    }
-                  </select>
-                </div>
-                <button onClick={ ()=> deleteThing(thing)}>x</button>
-                <button onClick={()=> increment(thing, -1)}>-</button>
-                <button onClick={()=> increment(thing, 1)}>+</button>
-                
-              </li>
-            );
-          })
+          things
+            .sort((t1, t2) => t2.ranking - t1.ranking)
+            .map( thing => {
+              const owner = usersWithNumThings.find(user => user.id === thing.userId) || {};
+              return (
+                <li key={ thing.id }>
+                  { thing.name } ({ thing.ranking })
+                  { owner.name && ` owned by ${owner.name} who has ${owner.numThingsOwned} thing${ (owner.numThingsOwned > 1) ? `s` : '' }` || `` }
+                  <div>
+                    <select defaultValue={ thing.userId } onChange={ ev => updateThing(thing, ev.target.value )}>
+                      <option value=''>-- nobody --</option>
+                      {
+                        usersWithNumThings
+                          .filter(user => user.numThingsOwned < 3 || user.id === thing.userId)
+                          .map( user => {
+                          return (
+                            <option key={ user.id } value={ user.id }>{ user.name }</option>
+                          );
+                        })
+                      }
+                    </select>
+                  </div>
+                  <button onClick={ ()=> deleteThing(thing)}>x</button>
+                  <button onClick={()=> increment(thing, -1)}>-</button>
+                  <button onClick={()=> increment(thing, 1)}>+</button>
+                  
+                </li>
+              );
+            })
         }
       </ul>
       <ThingForm />
